856c8de8e434c9c12be7441c41085719f945fab7
Magnus Grindal Bakken <magnusbakken@gmail.com>
2023-05-04 08:39:42 +0200

    Customize map

--- packages/client/src/app/map/map.component.ts
+++ packages/client/src/app/map/map.component.ts
@@ -1,8 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Marker } from './types';
 
 @Component({
   selector: 'screenguessr-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss'],
 })
-export class MapComponent {}
+export class MapComponent {
+  @Output() public readonly markerPlaced = new EventEmitter<Marker>();
+
+  public readonly options: google.maps.MapOptions = {
+    draggableCursor: 'crosshair',
+    fullscreenControl: false,
+    mapTypeControl: false,
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    streetViewControl: false
+  };
+
+  public marker: Marker | null = null;
+
+  public placeMarker(event: google.maps.MapMouseEvent | google.maps.IconMouseEvent): void {
+    if (event.latLng !== null) {
+      this.marker = { latitude: event.latLng.lat(), longitude: event.latLng.lng() };
+      this.markerPlaced.emit(this.marker);
+    }
+  }
+}
