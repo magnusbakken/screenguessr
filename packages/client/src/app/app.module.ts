2da2d909ef6ab6da7637429d5ec2dc3893b8cb44
Magnus Grindal Bakken <magnusbakken@gmail.com>
2023-05-04 08:13:42 +0200

    Added Google Maps

--- packages/client/src/app/app.module.ts
+++ packages/client/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { GoogleMapsModule } from '@angular/google-maps'
+
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, NxWelcomeComponent, MapComponent],
+  imports: [BrowserModule, GoogleMapsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
